79114eb449a9500fad7d1bc05b7b0f96c2840db8
Andrii Pavliuk <lv051187paa@gmail.com>
2018-09-19 13:03:06 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Offer from './components/Offer';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Offer/>
+      </div>
+    );
+  }
+}
+
+export default App;
